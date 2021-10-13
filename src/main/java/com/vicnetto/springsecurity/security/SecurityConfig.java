b8fc856e35ff1bda12ee51216967c4c33427c02f
vicnetto <victorhmnetto@gmail.com>
2021-10-13 19:59:56 -0300

    :sparkles: Adding a new class to secure the application
    This class SecurityConfig is going to help the application with the
    security. First, to log in now, a registered user should be entered.
    Currently, it's using basic auth (although it's not recommended), just
    for testing purpose.

---
+++ src/main/java/com/vicnetto/springsecurity/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.vicnetto.springsecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // Any request must be authenticated, with http basic.
+        http.authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails victor = User.builder()
+                .username("vicnetto")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(victor);
+    }
+}
