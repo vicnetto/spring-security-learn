a7df497c3761ca9fd7415a667b4c480085ba01ed
vicnetto <victorhmnetto@gmail.com>
2021-10-14 01:44:44 -0300

    :sparkles: Inserting user permissions and roles

---
+++ src/main/java/com/vicnetto/springsecurity/security/UserPermissions.java
@@ -0,0 +1,18 @@
+package com.vicnetto.springsecurity.security;
+
+public enum UserPermissions {
+    STUDENT_READ("student:read"),
+    STUDENT_WRITE("student:write"),
+    COURSE_READ("course:read"),
+    COURSE_WRITE("course:write");
+
+    private final String permission;
+
+    UserPermissions(String permission) {
+        this.permission = permission;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+}
---
+++ src/main/java/com/vicnetto/springsecurity/security/UserRole.java
@@ -0,0 +1,35 @@
+package com.vicnetto.springsecurity.security;
+
+import com.google.common.collect.Sets;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.vicnetto.springsecurity.security.UserPermissions.*;
+
+public enum UserRole {
+    ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_READ, STUDENT_WRITE)),
+    ADMINTRAINEE(Sets.newHashSet(COURSE_READ, STUDENT_READ)),
+    STUDENT(Sets.newHashSet());
+
+    private final Set<UserPermissions> rolePermissions;
+
+    UserRole(Set<UserPermissions> rolePermissions) {
+        this.rolePermissions = rolePermissions;
+    }
+
+    public Set<UserPermissions> getRolePermissions() {
+        return rolePermissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> authorities = rolePermissions.stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+
+        return authorities;
+    }
+}
